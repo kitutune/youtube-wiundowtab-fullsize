df2dca7a69a7be7b49c6de7c16bdd21c8d754e0b
kitutune <p.kitutune@gmail.com>
2021-09-04 23:17:58 +0900

    🐛 zipファイルの呼び出し部分を修正

--- components/dlLink/index.jsx
+++ components/dlLink/index.jsx
@@ -3,8 +3,8 @@ export const DlLink = (props) => {
     <>
       {!props.id ? (
         <a
-          href="YoutubeFullSizeWindow.zip"
-          download="YoutubeFullSizeWindow.zip"
+          href="YoutubeFullSizeWindow!!.zip"
+          download="YoutubeFullSizeWindow!!.zip"
         >
           <p className="pt-1 text-yellow-50 hover:text-yellow-300">
             DL Google Extensions（Unzip required）

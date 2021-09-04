9c2360f2060802370eac7f9315a961d5d635946d
kitutune <p.kitutune@gmail.com>
2021-09-04 15:42:43 +0900

    📱tailwindcssに縦横比16:9の要素を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '56.25%': '56.25%',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

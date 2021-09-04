ddea8967d412d8d78aab6c92cc6a716e9695aa70
kitutune <p.kitutune@gmail.com>
2021-09-04 16:37:44 +0900

    ➕google拡張機能のファイルを追加ver0.2.0

---
+++ googleExtension/YoutubeFullSizeWindow!!/openApp.js
@@ -0,0 +1,17 @@
+function openApp() {
+  let keyword = 'https://www.youtube.com/watch?v=';
+  let url = location.href;
+  url = ~url.indexOf('&') ? url.slice(0, url.indexOf('&')) : url;
+  if (~url.indexOf(keyword)) {
+    window.open(
+      url.replace(keyword, 'https://import-youtube-fullsize.vercel.app/')
+    );
+  }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: openApp,
+  });
+});
